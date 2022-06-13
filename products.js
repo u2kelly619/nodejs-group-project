91e2a8a84778bf185fdbf24ef9cb9abb612ab248
Clara0919 <mansyuan.he@gmail.com>
2022-06-13 17:17:13 +0800

    0613 修改category

--- products.js
+++ products.js
@@ -1,7 +1,7 @@
 module.exports = [
     {
         title: '黑芝麻花生脆餅',
-        category: '手工餅乾',
+        category: 'cookie',
         price: 250,
         description: '無麩質 天然純手工製作 低溫烘焙，非油炸 無添加防腐劑, 無香料, 無蔗糖 保留完整營養 嚴選天然飽滿日本黑芝麻 搭配低溫烘焙台灣台農11號花生 雙重口味的撞擊味蕾 細緻酥脆的口感',
         imageUrlOne: 'https://cdn1.cybassets.com/media/W1siZiIsIjIwODgzL3Byb2R1Y3RzLzM2MDI1MDg5LzE2NDI2MDY4NjNfNGEwNmY0NTQ5ODM5YWU2ZDBiMTAuanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=5a1a12c9d1643bc8',
@@ -10,7 +10,7 @@ module.exports = [
     },
     {
         title: '經典原味拿花曲奇',
-        category: '手工餅乾',
+        category: 'cookie',
         price: 260,
         description: '超美又酥鬆的擠花餅乾 也是店裡人氣不墜商品之一 減糖70% 酥酥鬆鬆好口感 烘焙是一種精彩的科學和藝術, 因為減糖, 就要精算其他粉類的黃金比例, 外在氣候環境濕度...... 等等的影響, 店裡拿花餅乾都有一群無敵愛好者支持, 在製作擠花曲奇餅乾也是我四年來進入烘焙世界最最舒壓開心的時光之一, 常常不知不覺就擠了滿滿三大盤, 美美的出場啊~~~',
         imageUrlOne: 'https://cdn1.cybassets.com/media/W1siZiIsIjIwODgzL3Byb2R1Y3RzLzM2MDI1MDk4LzE2NTQwMTE5NjlfODE1YzM2NDRmZGUxMWM3ZTlmZGIuanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=fed1655f6985ea4a',
@@ -19,7 +19,7 @@ module.exports = [
     },
     {
         title: '濃情可可酥餅',
-        category: '手工餅乾',
+        category: 'cookie',
         price: 260,
         description: '尖叫聲!!! 璐緹 [香濃可可曲奇] 華麗登場 酥酥鬆鬆 可可對人體的好處很多, 這款帶有苦巧克力的獨特風味, 讓你安心輕鬆吃以及少糖少油的零嘴, 也是我們店內大人小孩都滿意的手工餅乾之一, 常常是家裡小朋友指定的常備點心之一. 可可的好處還提供身體營養元素之一, 抗發炎防失智唷!',
         imageUrlOne: 'https://cdn1.cybassets.com/media/W1siZiIsIjIwODgzL3Byb2R1Y3RzLzM2MjYyNzY1LzE2NTM0NzUxMzdfYTUxZTU2MmNkNzMyNWYyZmY0NjAuanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=96393893d1386161',
@@ -28,7 +28,7 @@ module.exports = [
     },
     {
         title: '綜合曲奇餅乾罐',
-        category: '手工餅乾',
+        category: 'cookie',
         price: 270,
         description: '想到零食 你絕對不能少了它！ 滿滿一大罐 各式規格種類餅乾 值得推薦 防疫健康糧食 減糖版本 下午茶零食/辦公室團購/會議點心/伴手禮.. 完全大大滿足你的需求',
         imageUrlOne: 'https://cdn1.cybassets.com/media/W1siZiIsIjIwODgzL3Byb2R1Y3RzLzM2MDIwOTgzLzE2NTE0NzYyNDVfYjUyZjE0MzcyNDA2ZGQ5MmJjZGIuanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=b1fd53574d2bb623',
@@ -37,7 +37,7 @@ module.exports = [
     },
     {
         title: '手工南瓜子海苔堅果薄片',
-        category: '手工餅乾',
+        category: 'cookie',
         price: 270,
         description: '酥酥脆脆的口感, 灑上海苔粉, 是這款點心的一大亮點, 再搭配滿滿的南瓜子, 每一口都幸福啊~',
         imageUrlOne: 'https://cdn1.cybassets.com/media/W1siZiIsIjIwODgzL3Byb2R1Y3RzLzM2Nzg1MjU0LzE2NTIyNzI3NjJfYTE2YWQ2YWZmNTE1OGRkOWFiNzEuanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=3e63bb3912abc470',
@@ -46,7 +46,7 @@ module.exports = [
     },
     {
         title: '手工杏仁花生堅果薄片',
-        category: '手工餅乾',
+        category: 'cookie',
         price: 280,
         description: '六年多我們累積了一群鐵粉, 這款堅果薄片, 也是鐵粉們必備的零食之一 酥酥脆脆的口感, 搭配滿滿的杏仁跟花生堅果, 每一口都幸福啊~ 堅持純手作 減糖70% 一直是我們的堅持 最要謝謝忠實顧客一直以來的支持與照顧',
         imageUrlOne: 'https://cdn1.cybassets.com/media/W1siZiIsIjIwODgzL3Byb2R1Y3RzLzM2Nzg1MjUwLzE2NTIyNzI1MDFfZDQwM2FlNTVjYTk0ZTViYjg2YzYuanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=0591dd454c16999b',
@@ -55,7 +55,7 @@ module.exports = [
     },
     {
         title: '杏仁核桃奶酥餅乾',
-        category: '手工餅乾',
+        category: 'cookie',
         price: 290,
         description: '核核營養價值豐富堪稱堅果類之王 也是養生之寶 這款餅乾也是減糖版, 深受小朋友喜歡, 媽媽們的口袋第一名商品, 口感酥脆, 滿滿的核桃, 越是咀嚼越發香濃厚實 店裡回購率最高商品之一唷~',
         imageUrlOne: 'https://cdn1.cybassets.com/media/W1siZiIsIjIwODgzL3Byb2R1Y3RzLzM2MDA5MjY3LzE2NTIyNzIxMDlfNDk1ODA0NmVlZDIwYTUzNzkwOWIuanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=461824634b9e9d62',
@@ -64,7 +64,7 @@ module.exports = [
     },
     {
         title: '香濃抹茶杏仁酥餅',
-        category: '手工餅乾',
+        category: 'cookie',
         price: 330,
         description: '大家都愛的幸福味道 我的一群抹茶控顧客們, 總是不忘給我功課, 激發我的爆發力~~~ 來來來, 這款抹茶造型樸實無華, 我添加了杏仁角, 增加酥脆口感, 一口咬住的當下, 香淳濃郁的滋味實在難以忘懷. 只是現在現磨抹茶粉食材實在太貴啦, 大家省著吃啊呀呀~~~~ 抹茶控們 快快照過來!!!',
         imageUrlOne: 'https://cdn1.cybassets.com/media/W1siZiIsIjIwODgzL3Byb2R1Y3RzLzM2OTU2MzYxLzE2NTM1NzA0OThfM2FmMTFmMzc5MzEwZjUyZGZhZDguanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=e6b1055769d65d65',
@@ -73,7 +73,7 @@ module.exports = [
     },
     {
         title: '燕麥胚芽堅果餅',
-        category: '手工餅乾',
+        category: 'cookie',
         price: 330,
         description: '高比例燕麥搭配豐富的堅果 高纖少糖美味 營養又飽足 堅持少糖少油純手作餅乾 很多客戶會買回去給家裡長輩當下午茶點心 這款非常適合血糖控制和講求低碳水化合物健康飲食的朋友們 採用最簡單最快速的方式 將所有材料研磨成一定的大小細度 讓大人們享受下午茶時光 讓家裡大人們一起享受美味的減糖點心',
         imageUrlOne: 'https://cdn1.cybassets.com/media/W1siZiIsIjIwODgzL3Byb2R1Y3RzLzM2MDIwOTcwLzE2NTM0NzUxOTlfMGUwODdkNGE1MDUzMjNkZTU0ZjYuanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=a1fbf935e91af184',
@@ -82,7 +82,7 @@ module.exports = [
     },
     {
         title: '璐緹嚴選掛耳咖啡5入裝',
-        category: '職人咖啡',
+        category: 'coffee',
         price: 160,
         description: '進口精品咖啡生豆 自家職人烘焙師 堅果, 濃郁厚實, 帶著綜合苦甜巧克力的風味',
         imageUrlOne: 'https://cdn1.cybassets.com/media/W1siZiIsIjIwODgzL3Byb2R1Y3RzLzM2MDIxMTM2LzE2NDI0NzUyOThfNzI5N2IyZTQwMThkMjNjMTFlYjYuanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=c83dd0eff64386e2',
@@ -91,7 +91,7 @@ module.exports = [
     },
     {
         title: '掛耳咖啡10入彩盒裝',
-        category: '職人咖啡',
+        category: 'coffee',
         price: 400,
         description: '璐緹嚴選掛耳咖啡10入裝 進口精品咖啡生豆 自家職人烘焙師 堅果, 濃郁厚實, 帶著綜合苦甜巧克力的風味',
         imageUrlOne: 'https://cdn1.cybassets.com/media/W1siZiIsIjIwODgzL3Byb2R1Y3RzLzM2ODI4MDQ0LzE2NTE5MTI2NTdfMDYzN2Y0MzQ2YjdjMjRkYTA4ZGMuanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=93aa59d59f730549',
@@ -100,7 +100,7 @@ module.exports = [
     },
     {
         title: '璐緹嚴選綜合豆',
-        category: '職人咖啡',
+        category: 'coffee',
         price: 350,
         description: '精品綜合豆 中美洲加非洲豆多種咖啡豆 中深烘焙 風味介紹:奶油 香草 焦糖巧克力',
         imageUrlOne: 'https://cdn1.cybassets.com/media/W1siZiIsIjIwODgzL3Byb2R1Y3RzLzM2MDIyMjYxLzE2NDI0OTEwNjBfYzQzMWExYTk2NzI1MDQ5ZTBlZjMuanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=538213d5c9c659f4',
@@ -109,7 +109,7 @@ module.exports = [
     },
     {
         title: '精品曼巴綜合',
-        category: '職人咖啡',
+        category: 'coffee',
         price: 370,
         description: '精品綜合豆 獨特經典綜合咖啡豆 中等稍深烘焙 風味介紹:堅果可可香甜清甜溫潤回甘',
         imageUrlOne: 'https://cdn1.cybassets.com/media/W1siZiIsIjIwODgzL3Byb2R1Y3RzLzM2MDI1MTcwLzE2NDI1NzczMTFfZWZiY2U0ODVkMjFlYTlhZWZlMjYuanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=21350df13e875911',
@@ -118,7 +118,7 @@ module.exports = [
     },
     {
         title: '衣索比亞耶加雪菲',
-        category: '職人咖啡',
+        category: 'coffee',
         price: 390,
         description: '精品咖啡豆 孔加產地 日曬 G1 風味介紹:橘香 草莓般的酸甜感 餘韻甜美',
         imageUrlOne: 'https://cdn1.cybassets.com/media/W1siZiIsIjIwODgzL3Byb2R1Y3RzLzM2MDI1MTM5LzE2NDI1NzYyMTNfNGJhZTIzOTdkMWExOTY4MjJkNjguanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=17a9dd10fe2dc5ca',
@@ -127,7 +127,7 @@ module.exports = [
     },
     {
         title: '新幾內亞天堂鳥AA',
-        category: '職人咖啡',
+        category: 'coffee',
         price: 400,
         description: '精品咖啡豆 波旁品種 水洗 風味介紹：白柚, 花香, 堅果, 白茶',
         imageUrlOne: 'https://cdn1.cybassets.com/media/W1siZiIsIjIwODgzL3Byb2R1Y3RzLzM2MDI1MTg2LzE2NDMyNTkyNTVfMGYwNGZjNmIyMzllMjU1Nzg3Y2UuanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=7274deb198d3bff0',
@@ -136,7 +136,7 @@ module.exports = [
     },
     {
         title: '印尼黃金曼特寧',
-        category: '職人咖啡',
+        category: 'coffee',
         price: 420,
         description: '蘇門答臘 林東地區 LINTONG 2爆初 中深烘焙 半水洗濕脫殼 風味介紹：奶油、柑橘、焦糖、巧克力',
         imageUrlOne: 'https://cdn1.cybassets.com/media/W1siZiIsIjIwODgzL3Byb2R1Y3RzLzM2MDI1MTgxLzE2NDMzODg4MzNfZDdkMTMyNGZmNWRmNjcxZTdkMzQuanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=2c99c1d178164cad',
@@ -145,7 +145,7 @@ module.exports = [
     },
     {
         title: '愛樂壓Go咖啡冲煮神器',
-        category: '沖煮器具',
+        category: 'tool',
         price: 1080,
         description: '類似注射器的簡易咖啡煮具，結合了滴漏和濾壓式咖啡機的特點。簡單、耐用、方便攜帶。沖泡出的咖啡口感濃郁，且仍能保留其中細緻的風味。',
         imageUrlOne: 'https://cdn1.cybassets.com/media/W1siZiIsIjIwODgzL3Byb2R1Y3RzLzM2MDI1MjI1LzE2NDI1Nzc2MDRfZjhhYWU2NjVjMjgzZDMwMDM0OGUuanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=e2c68c0d1a1748f0',
@@ -154,7 +154,7 @@ module.exports = [
     },
     {
         title: '愛樂壓專用濾紙',
-        category: '沖煮器具',
+        category: 'tool',
         price: 195,
         description: 'AeroPress 愛樂壓 100%美國製造 原裝進口專用特殊濾紙',
         imageUrlOne: 'https://cdn1.cybassets.com/media/W1siZiIsIjIwODgzL3Byb2R1Y3RzLzM2MTAzODk2LzE2NDQyMzkyNjJfNDVjNjY2NDZkY2U5YzZhODI1YTIuanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=f9dfa0cbf4dea671',
@@ -163,7 +163,7 @@ module.exports = [
     },
     {
         title: 'AdHoc陶瓷刀咖啡豆研磨機',
-        category: '沖煮器具',
+        category: 'tool',
         price: 3280,
         description: '獨家陶瓷刀具，研磨均勻，完整保留咖啡香味',
         imageUrlOne: 'https://cdn1.cybassets.com/media/W1siZiIsIjIwODgzL3Byb2R1Y3RzLzM2MDI1MjM4LzE2NDI1Nzc5MDVfODgyZGIzODU5NjU4NTI4MGQ1YzguanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=6d35c437a1d3011d',
@@ -172,7 +172,7 @@ module.exports = [
     },
     {
         title: 'AdHoc無段式不鏽鋼手沖咖啡架',
-        category: '沖煮器具',
+        category: 'tool',
         price: 2980,
         description: '無段式不鏽鋼手沖咖啡架',
         imageUrlOne: 'https://cdn1.cybassets.com/media/W1siZiIsIjIwODgzL3Byb2R1Y3RzLzM2MDI1MjMzLzE2NDI1Nzc4MjdfZjc0Mzk1MDAxMGE1YTljMjc5OGQuanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=63b713b7a45e5d49',
@@ -181,7 +181,7 @@ module.exports = [
     },
     {
         title: 'Adhoc法式雙層玻璃咖啡濾壓杯',
-        category: '沖煮器具',
+        category: 'tool',
         price: 1790,
         description: '以法式濾壓的方式沖泡，能快速萃取出咖啡的原始風味',
         imageUrlOne: 'https://cdn1.cybassets.com/media/W1siZiIsIjIwODgzL3Byb2R1Y3RzLzM2MTAzODkxLzE2NDQyMzg5MTJfMjc3NDVhMWRhY2M0ZjZiYzRhNDMuanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=95f269f43f1d4472',
@@ -190,7 +190,7 @@ module.exports = [
     },
     {
         title: '奶泡/醬料多功能電動攪拌器',
-        category: '沖煮器具',
+        category: 'tool',
         price: 1780,
         description: '奶泡/醬料多功能電動攪拌器',
         imageUrlOne: 'https://cdn1.cybassets.com/media/W1siZiIsIjIwODgzL3Byb2R1Y3RzLzM2MDI1MjY5LzE2NDI1NzgxNTRfMmU0ZDIxYzg4NDZmYWVkYmFjNTYuanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=98d8f04710834d3b',
@@ -199,7 +199,7 @@ module.exports = [
     },
     {
         title: '包種茶立體茶包',
-        category: '精選好物',
+        category: 'selection',
         price: 35,
         description: '選用台灣特色包種 適合大餐後的去油解膩茶飲 也是新春跟長輩長官拜年的體面最佳伴手禮 台灣唯一有香氣指紋的包種',
         imageUrlOne: 'https://cdn1.cybassets.com/media/W1siZiIsIjIwODgzL3Byb2R1Y3RzLzM2MDI1OTY3LzE2NDU0NDc3OTNfNmJjZTFjNDBmNjZlYzM4ZjAwZWIuanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=f5768fdbdb479de6',
@@ -208,7 +208,7 @@ module.exports = [
     },
     {
         title: '包種茶彩盒裝(10入裝)',
-        category: '精選好物',
+        category: 'selection',
         price: 360,
         description: '我喜歡叫他博士的茶，他是立體茶包，冷泡熱泡都好喝。',
         imageUrlOne: 'https://cdn1.cybassets.com/media/W1siZiIsIjIwODgzL3Byb2R1Y3RzLzM2Nzg1NjU0LzE2NTE0NzcyMzFfYzAxMWYxM2U4YjQxZDlhNjUwMmUuanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=6f31b7f24cd3ecde',
@@ -217,7 +217,7 @@ module.exports = [
     },
     {
         title: '包種茶經濟裸包(20入裝)',
-        category: '精選好物',
+        category: 'selection',
         price: 390,
         description: '台灣唯一有香味指紋的經典包種茶 經濟包/20入/裸包',
         imageUrlOne: 'https://cdn1.cybassets.com/media/W1siZiIsIjIwODgzL3Byb2R1Y3RzLzM2MTI0NTk5LzE2NDU0NDc2MzRfMGQ4ZTUwNTcxYjQyYWYyNTM2ZGMuanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=6cf07426f0198739',
@@ -226,7 +226,7 @@ module.exports = [
     },
     {
         title: '多用途茶包杯蓋組(含座)',
-        category: '精選好物',
+        category: 'selection',
         price: 890,
         description: '創新設計可固定茶包吊繩 創新美感與功能性兼具',
         imageUrlOne: 'https://cdn1.cybassets.com/media/W1siZiIsIjIwODgzL3Byb2R1Y3RzLzM2MzM3NzA0LzE2NDYzMTAwODFfZDJiZTJhNzE5MTFkMzhiMGVkODYuanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=740077fe27a7ef96',
@@ -235,7 +235,7 @@ module.exports = [
     },
     {
         title: '璐緹幸福好禮',
-        category: '伴手禮盒',
+        category: 'gift',
         price: 670,
         description: '罐裝手工餅乾2罐(口味隨機) 手工餅乾常溫保存45天',
         imageUrlOne: 'https://cdn1.cybassets.com/media/W1siZiIsIjIwODgzL3Byb2R1Y3RzLzM2MDI1Mjc3LzE2NDI1NzgzODJfZDE3YWFlNDYxYjJmMDRkOWZlMjUuanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=f74b188a322c62b7',
@@ -244,7 +244,7 @@ module.exports = [
     },
     {
         title: '璐緹金喜好禮',
-        category: '伴手禮盒',
+        category: 'gift',
         price: 790,
         description: '罐裝手工餅乾1罐(口味隨機） 璐緹精選掛耳咖啡10包入 手工餅乾常溫保存45天 咖啡掛耳包最佳賞味期60天 可保存10個月',
         imageUrlOne: 'https://cdn1.cybassets.com/media/W1siZiIsIjIwODgzL3Byb2R1Y3RzLzM2MDI1MjgzLzE2NDI1Nzg2MzJfNjQwYmY4ZjJlODZiZWZkOWYyZjUuanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=b7728dc021c48d1e',
@@ -253,7 +253,7 @@ module.exports = [
     },
     {
         title: '璐緹時尚好禮',
-        category: '伴手禮盒',
+        category: 'gift',
         price: 890,
         description: '咖啡掛耳/浸泡茶葉 2盒 1盒10入裝',
         imageUrlOne: 'https://cdn1.cybassets.com/media/W1siZiIsIjIwODgzL3Byb2R1Y3RzLzM2MDI1Mjk0LzE2NDI1Nzg3MjRfNjZlYTY4MDBkN2E3YmJiZDRhMGEuanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=25a9e538697333a5',
@@ -262,7 +262,7 @@ module.exports = [
     },
     {
         title: '璐緹經典獨家好禮',
-        category: '伴手禮盒',
+        category: 'gift',
         price: 950,
         description: '[頂級]行家首選濾掛咖啡禮盒 24入組 你的每日手沖精品 溫暖你的每一天 自家職人直火烘培 更多層次的濃郁厚實風味',
         imageUrlOne: 'https://cdn1.cybassets.com/media/W1siZiIsIjIwODgzL3Byb2R1Y3RzLzM2MDI1MzAyLzE2NDI1Nzg4NzVfYjExNTlhM2YwMzg2NzlhMTM0NjQuanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=5d72e0eee2283340',
@@ -271,7 +271,7 @@ module.exports = [
     },
     {
         title: '璐緹富貴好禮',
-        category: '伴手禮盒',
+        category: 'gift',
         price: 950,
         description: '選用台灣特色包種 適合大餐後的去油解膩茶飲 也是新春跟長輩長官拜年的體面最佳伴手禮 台灣唯一有香氣指紋的包種 24入組',
         imageUrlOne: 'https://cdn1.cybassets.com/media/W1siZiIsIjIwODgzL3Byb2R1Y3RzLzM2MDI1MzEzLzE2NDI1NzkyMjVfZTlkYzYwYjQ0MjdiOTQwNGQ0MDkuanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=dc942e33b3348a83',
@@ -280,7 +280,7 @@ module.exports = [
     },
     {
         title: '璐緹雙享好禮',
-        category: '伴手禮盒',
+        category: 'gift',
         price: 950,
         description: '一份好禮同時滿足 送禮自用都實用 精緻典雅品味非凡 12入咖啡掛耳 12入浸泡茶葉',
         imageUrlOne: 'https://cdn1.cybassets.com/media/W1siZiIsIjIwODgzL3Byb2R1Y3RzLzM2MDI1MzIwLzE2NDI1NzkzNjRfMTUxNGM5ZGM3ZjM0ZTAxN2U5MWEuanBlZyJdLFsicCIsInRodW1iIiwiNjAweDYwMCJdXQ.jpeg?sha=b93cc5c1a49868d3',
